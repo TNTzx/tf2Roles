c98c8dbc1be727e4f4f91ea4ff2dd1c19932f4b5
TNTzx <xanderandrewritua@gmail.com>
2023-01-19 21:06:29 +0800

    added imports in libraries

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
 """A program used for managing claimable roles in RDL and 7th Beat Cafe."""
+
+
+
+from .cogs import *
+from .global_vars import *
+from .utils import *
+
+from .tf2main import *
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,5 @@
 """Contains all utilities that can be used in the program."""
+
+
+from .role_manage import *
+from .translation import *
