0d3a5228add9d11ce34ea109519654d7ea3b3ab8
TNTzx <xanderandrewritua@gmail.com>
2023-01-19 21:07:01 +0800

    finished role viewing, still needs docstrings

--- cogs/view_roles.py
+++ cogs/view_roles.py
@@ -5,20 +5,21 @@ import disnake
 from disnake.ext import commands
 
 from .. import global_vars
+from .. import utils
 
 
 
-global_vars = global_vars.Globals.get_globals()
+GLOBALS = global_vars.Globals.get_globals()
 
 
 class CogViewRole(commands.Cog):
     """Contains all commands relating to viewing roles."""
 
-    @global_vars.bot.user_command(name='View Roles', guild_ids=global_vars.guilds)
+    @GLOBALS.bot.user_command(name='View Roles', guild_ids=GLOBALS.guilds)
     async def view_role_context(self, inter: disnake.UserCommandInteraction):
-        await _roles(inter, type = 'Role', user = inter.target)
+        await utils.display_roles(inter, display_type = utils.DisplayTypes.ROLE, user = inter.target)
 
 
-    @global_vars.bot.user_command(name='View Role Icons', guild_ids=global_vars.guilds)
+    @GLOBALS.bot.user_command(name='View Role Icons', guild_ids=GLOBALS.guilds)
     async def view_roleicon_context(self, inter):
-        await _roles(inter, type='Icon', user=inter.target)
+        await utils.display_roles(inter, display_type = utils.DisplayTypes.ROLE_ICON, user = inter.target)
